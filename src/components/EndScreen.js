35e501d5c48b456d4b8245b2864935036687a982
sansa1308 <sanskritirani96@gmail.com>
2021-11-07 14:44:06 +0530

    Initial Commit

---
+++ src/components/EndScreen.js
@@ -0,0 +1,25 @@
+import React, { useState, useContext } from "react";
+import { QuizContext } from "../helpers/Context";
+import { Questions } from "../helpers/QuestionBank";
+import "../App.css";
+
+const EndScreen = () => {
+  const { score, setScore, setGameState } = useContext(QuizContext);
+
+  const restartQuiz = () => {
+    setScore(0);
+    setGameState("menu");
+  };
+
+  return (
+    <div className="EndScreen">
+      <h1>Quiz Finished</h1>
+      <h3>
+        {score} / {Questions.length}
+      </h3>
+      <button onClick={restartQuiz}>Restart Quiz</button>
+    </div>
+  );
+};
+
+export default EndScreen;
---
+++ src/helpers/QuestionBank.js
@@ -0,0 +1,26 @@
+export const Questions = [
+  {
+    prompt: "what is 1+3?",
+    optionA: "24",
+    optionB: "21",
+    optionC: "4",
+    optionD: "2",
+    answer: "C",
+  },
+  {
+    prompt: "what is a Streaming Platform?",
+    optionA: "Twitter",
+    optionB: "Twitch",
+    optionC: "Google",
+    optionD: "Robinhood",
+    answer: "B",
+  },
+  {
+    prompt: "what goes with Flora?",
+    optionA: "Fauna",
+    optionB: "Tree",
+    optionC: "Forest",
+    optionD: "Sky",
+    answer: "A",
+  },
+];
